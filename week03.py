2c9c76e752f8b716d634964e79782662efcdb660
KIMMINSOO-cr <kms0301081@naver.com>
2025-03-24 15:17:54 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,9 +1,11 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
- # ratings = [1, 2, 4, 3, 100]
- ratings = [1, 2, 4, 3]
- 
- group_rating = list(zip(groups, ratings))
- print(group_rating)
- cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
- cities = set(cities)
- print(cities)
+def inters(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
+
+
+l1 = [45,5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
+print(inters(l1, l2))
