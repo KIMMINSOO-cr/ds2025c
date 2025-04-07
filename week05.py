f96dda6e4a26e8dd0f116ed62c5cd6eacde1959f
KIMMINSOO-cr <kms0301081@naver.com>
2025-04-07 15:24:09 +0900

    v2.2 stack

--- week05.py
+++ week05.py
@@ -6,11 +6,8 @@ class Node:
  
  class Stack:
      def __init__(self):
-         self.items = list()
          self.top = None
  
-     def push(self, item):
-         self.items.append(item)
      def push(self, data):
          node = Node(data)
          if self.top is None:
@@ -20,35 +17,22 @@ class Node:
              self.top = node  # top update
  
      def pop(self):
-         return self.items.pop()
- 
-     def size(self):
-         return len(self.items)
- 
-     def is_empty(self):
-         return len(self.items) == 0
- 
-     def peek(self):
-         return self.items[-1]
          if self.top is None:
              return "Stack is empty!"
          popped_node = self.top
          self.top = self.top.link
+         popped_node.link = None
          return popped_node.data
  
  
  s1 = Stack()
- s2 = Stack()
- print(s1.is_empty())
  print(s1.pop())
+ # print(s1.pop())
  s1.push("Data structure")
- print(s1.is_empty())
- print(s2.is_empty())
  s1.push("Database")
- print(s1.size())
- print(s1.peek())
- print(s1.size())
  print(s1.pop())
- print(s1.size())
- print(s1.peek())
  print(s1.pop())
+ # print(s1.pop())
+ # print(s1.pop())
+ for i in range(3):
+     print(s1.pop())
