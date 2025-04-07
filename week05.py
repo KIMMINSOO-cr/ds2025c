583c7e306df099e312876cd12ae6ffea4c0a545f
KIMMINSOO-cr <kms0301081@naver.com>
2025-04-07 15:39:31 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,48 +1,16 @@
-class Node:
-     def __init__(self, data):
-         self.data = data
-         self.link = None
- 
- 
- class Stack:
-     def __init__(self):
-         self.top = None
- 
-     def push(self, data):
-         node = Node(data)
-         if self.top is None:
-             self.top = node
-         else:
-             node.link = self.top
-             self.top = node  # top update
- 
-     def pop(self):
-         if self.top is None:
-             return "Stack is empty!"
-         popped_node = self.top
-         self.top = self.top.link
-         popped_node.link = None
-         return popped_node.data
- 
- 
- s1 = Stack()
- # print(s1.pop())
- s1.push("Data structure")
- s1.push("Database")
- # print(s1.pop())
- # print(s1.pop())
- for i in range(3):
-     print(s1.pop())
- s1 = list()
- print(len(s1))
- s1.append("Data structure")  # push
- s1.append("Database")  # push
- print(len(s1))  # size
- print(s1[-1])  # peek
- print(s1)
- print(s1.pop())
- print(s1)
- print(s1.pop())
- print(s1)
- print(s1.pop())
- print(s1)
+
+def check_parentheses(a_string : str) -> bool:
+    stack =[]
+    for c in a_string:
+        if c=="(":
+            stack.append("c")
+        if c ==")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+check_parentheses("(2+3)")
+check_parentheses("(2+(2+3))")
+check_parentheses("(2+(2+3)")
+check_parentheses("+2_(2+3)(")
