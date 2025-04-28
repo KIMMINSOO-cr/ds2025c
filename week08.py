ac8634e7866413bb47e91cc4e9bb12903b5116ba
KIMMINSOO-cr <kms0301081@naver.com>
2025-04-28 15:30:27 +0900

    v3.2 tree

--- week08.py
+++ week08.py
@@ -1,39 +1,60 @@
 class TreeNode:
-    def __init__(self):
-        self.left = None
-        self.data = None
-        self.right = None
-
-def post_order(node):
-    if node:
-        post_order(node.left)
-        post_order(node.right)
-        print(node.data, end='-')
-
-if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9]
-    root = None
-
-    node = TreeNode()
-    node.data = numbers[0]
-    root = node
-
-    #2번째 원소 부터 마지막 원소까지
-    for number in numbers[1:]:
-        node = TreeNode()
-        node.data = number
-        current = root
-        while True:
-            if number < current.data:
-                if current.left is None:
-                    current.left = node
-                    break
-                current = current.left
-            else:
-                if current.right is None:
-                    current.right = node
-                    break
-                current = current.right
-
-post_order(root)
-
+     def __init__(self):
+         self.left = None
+         self.data = None
+         self.right = None
+ 
+ 
+ def post_order(node):
+     if node:
+         post_order(node.left)
+         post_order(node.right)
+         print(node.data, end='->')
+ 
+ 
+ if __name__ == "__main__":
+     numbers = [10, 15, 8, 3, 9]
+     root = None
+ 
+     node = TreeNode()
+     node.data = numbers[0]
+     root = node
+ 
+     # 2번째 원소 부터 마지막 원소까지
+     for number in numbers[1:]:
+         node = TreeNode()
+         node.data = number
+         current = root
+         while True:
+             if number < current.data:
+                 if current.left is None:
+                     current.left = node
+                     break
+                 current = current.left  # move
+             else:
+                 if current.right is None:
+                     current.right = node
+                     break
+                 current = current.right  # move
+ 
+     print("BST 구성 완료")
+ 
+     post_order(root)
+ 
+     find_number = int(input())
+     current = root
+     while True:
+         if find_number == current.data:
+             print(f"{find_number}을(를) 찾았습니다")
+             break
+         elif find_number < current.data:
+             if current.left is None:
+                 print(f"{find_number}이(가) 존재하지 않습니다")
+                 break
+             current = current.left
+         else:
+             if current.right is None:
+                 print(f"{find_number}이(가) 존재하지 않습니다")
+                 break
+             current = current.right
+ 
